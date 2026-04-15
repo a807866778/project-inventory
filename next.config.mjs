a1923c587949a55221924b5c57782ff465382679
a807866778 <74185447+a807866778@users.noreply.github.com>
2026-04-15 15:34:11 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
       bodySizeLimit: "5mb",
     },
   },
-  // Skip build-time page generation for pages that use database
-  pagesDir: false,
 };
 
 export default nextConfig;
